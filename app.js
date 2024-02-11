9bbb0a797d5f6feabd22163733504d82ec5b45ee
Anne Heinrich <67550502+annehxx@users.noreply.github.com>
2024-02-11 16:50:17 +0100

    Change background according to weather

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const apiUrl =
 const search = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 
+const weatherIcon = document.querySelector(".spline");
+
+const body = document.querySelector("body");
+
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
@@ -16,6 +20,27 @@ async function checkWeather(city) {
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
   document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+  if (data.weather[0].main == "Clouds") {
+    document.body.style.backgroundImage = "url('images/Default_high_quality_high_detailed_illustration_cloudy_day_1.jpg')";
+    
+  }
+  
+  if(data.weather[0].main === "Rain") {
+    document.body.style.backgroundImage = "url('images/Default_high_quality_high_detailed_illustration_rainy_cloudy_d_1.jpg')";
+  }
+
+  if(data.weather[0].main === "Clear") {
+    document.body.style.backgroundImage = "url('images/Default_high_quality_high_detailed_illustration_sunny_day_1.jpg')";
+  }
+
+  if(data.weather[0].main === "Drizzle") {
+    document.body.style.backgroundImage = "url('images/Default_high_quality_high_detailed_illustration_rainy_cloudy_d_1.jpg')";
+  }
+
+  if(data.weather[0].main === "Mist") {
+    document.body.style.backgroundImage = "url('images/Default_high_quality_high_detailed_illustration_mist_fog_day_i_1.jpg')";
+  }
 }
 
 searchBtn.addEventListener("click", () => {
